afce431c6acab45f5bcd32db2ae89c05532d5cef
newforce42 <alx.fr@outlook.com>
2019-12-10 10:15:42 -0500

    making script to add dummy data

---
+++ BangazonAPI/Model/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Model
+{
+    public class Customer
+    {
+    }
+}
---
+++ BangazonAPI/dummyData.sql
@@ -0,0 +1,3 @@
+﻿DELETE FROM Computer
+
+INSERT INTO Computer (PurchaseDate, DecomissionDate, Make, Manufacturer, Archived) VALUES ('20120618 10:34:09 AM', '20120922 12:22:55 AM', 'Mac', 'Apple', 'true')
