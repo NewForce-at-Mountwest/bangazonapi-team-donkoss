0462f0fb91e1ea61af33f4d50a225524cd7ff526
kelli-evans09 <kelli.evans09@yahoo.com>
2019-12-10 13:21:45 -0500

    successfully developed seed data for database

---
+++ BangazonAPI/SQLQuery5.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO PaymentType (AcctNumber, Name, CustomerId, Archived) VALUES ('123', 'Credit Card', '1', 'false');
+INSERT INTO PaymentType (AcctNumber, Name, CustomerId, Archived) VALUES ('124', 'Check', '2', 'false');
+INSERT INTO PaymentType (AcctNumber, Name, CustomerId, Archived) VALUES ('125', 'Cash', '3', 'false');
+INSERT INTO PaymentType (AcctNumber, Name, CustomerId, Archived) VALUES ('126', 'Paypal', '4', 'false');
+
+INSERT INTO Product (ProductTypeId, CustomerId, Price, Title, Description, Quantity, Archived) VALUES ('1', '1', '300.00', 'TV', '42" LCD TV', '50', 'false');
+INSERT INTO Product (ProductTypeId, CustomerId, Price, Title, Description, Quantity, Archived) VALUES ('1', '1', '300.00', 'TV', '42" LCD TV', '50', 'false');
+INSERT INTO Product (ProductTypeId, CustomerId, Price, Title, Description, Quantity, Archived) VALUES ('1', '1', '300.00', 'TV', '42" LCD TV', '50', 'false');
+INSERT INTO Product (ProductTypeId, CustomerId, Price, Title, Description, Quantity, Archived) VALUES ('1', '1', '300.00', 'TV', '42" LCD TV', '50', 'false');
